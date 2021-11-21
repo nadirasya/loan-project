5723e8cdc5f135c79629b71efef731220908109f
nadira arasya <nadira.arasya.tif18@polban.ac.id>
2021-11-22 03:06:34 +0700

    Initialize account service

---
+++ account-service/src/test/kotlin/com/nadira/loanProject/accountservice/AccountServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nadira.loanProject.accountservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
